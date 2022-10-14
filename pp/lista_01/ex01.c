400c4100159ef7ccb87586e4bc5bafe6522231bc
Ihury <kewinihury@gmail.com>
2022-10-14 12:38:30 -0300

    exercício 1

---
+++ pp/lista_01/ex01.c
@@ -0,0 +1,12 @@
+// Faça um programa que leia um número inteiro e o imprima.
+#include <stdio.h>
+
+int main() {
+  int numero;
+
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &numero);
+  printf("O numero digitado foi: %d", numero);
+
+  return 0;
+}
